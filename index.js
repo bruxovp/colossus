a3162ef16a9873d366e68206dba45c4ebfbb534a
bruxovp <joao.pereira.santos08@escola.pr.gov.br>
2024-11-27 20:23:41 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+import { movies } from ´./movies.js';
+  document.getElementByld('filmFORM').ADDeVENTlISTENER('SUBMIT', FUNCTION (EVENT) {
+    EVENT.PREVENTdEFAULT(); // EVITA O ENVIO DO FORMULÁRIO
+    // obtém aidade e categorias selecionadas
+let age = parselnt(document.getelementbyld('age')
